a427cd2ebc0baf577ef2ebb42e3c1da3dc56831c
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2022-01-18 09:36:15 +0100

    Added a reset button, other html changes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const API_KEY = '6b1e7103'
+
 $("div.intro").append("<p>This is a jquery test!</p>").append("<p>This is another paragraph!</p>")
 
 $("input[type='text']").keyup(function () {
@@ -18,4 +20,20 @@ $("input[type='number']").keyup(function () {
     }
 }).blur(function () {
     this.style.border = "0 solid red"
+})
+
+$('#resetProductTableButton').click(() => {
+    let confirm = window.confirm('Are you sure? All data will be lost.')
+
+    if (confirm === true) {
+        const resetUrl = `https://wt.ops.labs.vu.nl/api22/${API_KEY}/reset`
+
+        $.ajax({ url: resetUrl }).done((response) => {
+            if (response.Success) {
+                $('#requestFeedback').text('Table reset successful!')
+            }
+        }).catch((error) => {
+            $('#requestError').text(`Table reset failed! ${error.responseJSON.Error}`)
+        })
+    }
 })
