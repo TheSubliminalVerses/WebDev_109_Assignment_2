11baad30e347e7c488efca7e1ee75da1815816bd
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2022-01-18 10:23:59 +0100

    Added a form for adding a product

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ $("input[type='number']").keyup(function () {
     this.style.border = "0 solid red"
 })
 
-$('#resetProductTableButton').click(() => {
+$('#reset-product-table-button').click(() => {
     let confirm = window.confirm('Are you sure? All data will be lost.')
 
     if (confirm === true) {
@@ -30,10 +30,91 @@ $('#resetProductTableButton').click(() => {
 
         $.ajax({ url: resetUrl }).done((response) => {
             if (response.Success) {
-                $('#requestFeedback').text('Table reset successful!')
+                showMessage($('#request-feedback'), 'Table reset successful!')
             }
         }).catch((error) => {
-            $('#requestError').text(`Table reset failed! ${error.responseJSON.Error}`)
+            showMessage($('#request-error'), `Table reset failed! ${error.responseJSON.Error}`)
         })
     }
+})
+
+$('#add-product-button').click(() => {
+    $('#add-product-form').submit()
+})
+
+// Toggles a message for 2.5 seconds in an element
+const showMessage = ($elm, message) => {
+    $elm.text(message).fadeIn(500).delay(2500).fadeOut(500);
+}
+
+const addInputError = (inputName, message) => {
+    $(`<span class="input-error">${message}</span>`).insertAfter(`input[name="${inputName}"]`)
+}
+
+$('#add-product-form').submit((form) => {
+    form.preventDefault();
+    const addUrl = `https://wt.ops.labs.vu.nl/api22/${API_KEY}`
+    $('.input-error').remove()
+    let isFormValid = true
+
+    let product = {
+        brand: $('input[name="brand"]').val(),
+        model: $('input[name="model"]').val(),
+        os: $('input[name="os"]').val(),
+        screensize: $('input[name="screensize"]').val(),
+        image: $('input[name="image"]').val(),
+    }
+
+    // Validate fields
+    if (!product.brand) {
+        addInputError('brand', 'Brand is required!')
+        isFormValid = false
+    }
+
+    if (!product.model) {
+        addInputError('model', 'Model is required!')
+        isFormValid = false
+    }
+
+    if (!product.os) {
+        addInputError('os', 'OS is required!')
+        isFormValid = false
+    }
+
+    if (!product.screensize) {
+        addInputError('screensize', 'Screen size is required!')
+        isFormValid = false
+    }
+    if (!product.image) {
+        addInputError('image', 'Image is required!')
+        isFormValid = false
+    } else {
+        var urlExpression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(urlExpression);
+
+        if (!product.image.match(regex)) {
+            addInputError('image', 'Invalid URL!')
+            isFormValid = false
+        }
+    }
+
+    if (!isFormValid) {
+        showMessage($('#request-error'), `Review input fields and try again!`)
+        return
+    }
+
+    $.ajax({
+        url: addUrl,
+        method: 'POST',
+        data: product
+    }).done((response) => {
+        if (response.URI) {
+            showMessage($('#request-feedback'), 'Product added successful!')
+            $("#add-product-form").trigger("reset");
+        }
+    }).catch((error) => {
+        showMessage($('#request-error'), `Failed to add product! ${error.responseJSON.Error}`)
+    })
+
+    return false
 })
