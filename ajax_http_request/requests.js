2b6f12648f282a488a8a73a5a930f13dc10591d1
Michael Polesensky <adrianpolesensky@gmail.com>
2022-01-20 14:10:33 +0100

    Reload table function added

--- ajax_http_request/requests.js
+++ ajax_http_request/requests.js
@@ -143,3 +143,28 @@ $('#add-product-form').submit((form) => {
         showMessage($('#request-error'), `Failed to add product! ${error.responseJSON.Error}`)
     })
 })
+
+$("input.restore").click(function () {
+    $.ajax({
+        url: "https://wt.ops.labs.vu.nl/api22/6b1e7103",
+        method: "GET",
+        responseType: "JSON"
+    }).done(response => {
+        for (let i = 0; i < response.length; i++) {
+            $("tbody.table-body").append(`
+                <tr>
+                    <td>${response[i]['brand']}</td>
+                    <td>${response[i]['model']}</td>
+                    <td>${response[i]['os']}</td>
+                    <td>${response[i]['screensize']}</td>
+                    <td class="product-image">
+                        <figure>
+                            <img src=${response[i]['image']} alt=${response[i]['brand']} ${response[i]['model']}>
+                            <figcaption>${response[i]['brand']} ${response[i]['model']}</figcaption>
+                        </figure>
+                    </td>
+                </tr>
+            `)
+        }
+    })
+})
